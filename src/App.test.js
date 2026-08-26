91a8e05b3fa800a651990798b063e0cc911f3801
pipe-mv <pipe.m.v@hotmail.com>
2026-08-26 16:23:27 +1000

    Add verified GitHub Pages deployment workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the song search form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByRole('heading', { name: /song search/i })
+  ).toBeInTheDocument();
 });
