b073b8b0aa7cd6399c551309620004cc875666e1
pipe-mv <pipe.m.v@hotmail.com>
2026-08-26 17:50:59 +1000

    Stop tracking installed dependencies

--- node_modules/@testing-library/user-event/dist/esm/event/wrapEvent.js
+++
@@ -1,8 +0,0 @@
-import { getConfig } from '@testing-library/dom';
-
-function wrapEvent(cb, // eslint-disable-next-line @typescript-eslint/no-unused-vars
-_element) {
-    return getConfig().eventWrapper(cb);
-}
-
-export { wrapEvent };
